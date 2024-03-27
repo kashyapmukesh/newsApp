fa38933284b890fcc6936969e73d4ecc16bffee8
Mukesh kashyap <65002391+kashyapmukesh@users.noreply.github.com>
2024-03-27 12:04:15 +0530

    Update App.py
    Updated client side view

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+import threading
+
 import streamlit as st
 from PIL import Image
 from bs4 import BeautifulSoup as soup
@@ -7,7 +9,7 @@ import io
 import nltk
 nltk.download('punkt')
 
-st.set_page_config(page_title='InNews🇮🇳: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
+st.set_page_config(page_title='Short News') #, page_icon='./Meta/newspaper.ico')
 
 
 def fetch_news_search_topic(topic):
@@ -22,10 +24,10 @@ def fetch_news_search_topic(topic):
 
 def fetch_top_news():
     site = 'https://news.google.com/news/rss'
-    op = urlopen(site)  # Open that site
-    rd = op.read()  # read data from site
-    op.close()  # close the object
-    sp_page = soup(rd, 'xml')  # scrapping data from site
+    open = urlopen(site)  # Open that site
+    urlData = open.read()  # read data from site
+    open.close()  # close the object
+    sp_page = soup(urlData, 'xml')  # scrapping data from site
     news_list = sp_page.find_all('item')  # finding news
     return news_list
 
@@ -52,11 +54,11 @@ def fetch_news_poster(poster_link):
 
 
 def display_news(list_of_news, news_quantity):
-    c = 0
+    counter = 0
     for news in list_of_news:
-        c += 1
+        counter += 1
         # st.markdown(f"({c})[ {news.title.text}]({news.link.text})")
-        st.write('**({}) {}**'.format(c, news.title.text))
+        st.write('**({}) {}**'.format(counter, news.title.text))
         news_data = Article(news.link.text)
         try:
             news_data.download()
@@ -71,33 +73,73 @@ def display_news(list_of_news, news_quantity):
                 unsafe_allow_html=True)
             st.markdown("[Read more at {}...]({})".format(news.source.text, news.link.text))
         st.success("Published Date: " + news.pubDate.text)
-        if c >= news_quantity:
+        if counter >= news_quantity:
             break
 
+# def CSS_Customizer():
+#     st.markdown(
+#         """
+#         <style>
+#         footer {visibility: hidden;}
+#         .stDeployButton {
+#             visibility: hidden;
+#         }
+#        .stToolbarActions
+#        {
+#             visibility: hidden;
+#        }
+#         </style>
+#         """,
+#         unsafe_allow_html=True
+#     )
 
 def run():
-    st.title("InNews🇮🇳: A Summarised News📰")
-    image = Image.open('./Meta/newspaper.png')
+    st.title(":white[Latest Short News For You]")
+    # threading.Thread(target=CSS_Customizer, args=()).start()
+    st.markdown(
+        """
+        <style>
+        footer {visibility: hidden;}
+        .stDeployButton {
+            visibility: hidden;
+        }
+       .stToolbarActions
+       {
+            visibility: hidden;
+       }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    # image = Image.open('./Meta/newspaper.png')
+
 
     col1, col2, col3 = st.columns([3, 5, 3])
 
     with col1:
-        st.write("")
+        st.write(f":white[Maximize News Coverage in Minimal Time]")
 
-    with col2:
-        st.image(image, use_column_width=False)
+    # with col2:
+    # st.image(image, use_column_width=False)
 
     with col3:
         st.write("")
-    category = ['--Select--', 'Trending🔥 News', 'Favourite💙 Topics', 'Search🔍 Topic']
-    cat_op = st.selectbox('Select your Category', category)
+    category = ['--Select--', 'Trending News', 'Favourite Topics', 'Search Topic']
+    str_temp = ":white[Select your Category]"
+    cat_op = st.selectbox(str_temp, category)
     if cat_op == category[0]:
         st.warning('Please select Type!!')
+
+    # TRENDING NEWS SECTION
     elif cat_op == category[1]:
-        st.subheader("✅ Here is the Trending🔥 news for you")
-        no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
+        st.subheader("Here is the Trending news for you")
+        no_of_news = 20
+        no_of_news = st.slider('Number of News:', min_value=5, max_value=250, step=5)
         news_list = fetch_top_news()
         display_news(news_list, no_of_news)
+
+    # FAVOURITE NEWS SECTION
     elif cat_op == category[2]:
         av_topics = ['Choose Topic', 'WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE',
                      'HEALTH']
@@ -106,28 +148,29 @@ def run():
         if chosen_topic == av_topics[0]:
             st.warning("Please Choose the Topic")
         else:
-            no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
+            no_of_news = st.slider('Number of News:', min_value=5, max_value=250, step=5)
             news_list = fetch_category_news(chosen_topic)
             if news_list:
-                st.subheader("✅ Here are the some {} News for you".format(chosen_topic))
+                st.subheader("Here are the some {} News for you".format(chosen_topic))
                 display_news(news_list, no_of_news)
             else:
                 st.error("No News found for {}".format(chosen_topic))
 
+    # SEARCH NEWS TOPIC SECTION
     elif cat_op == category[3]:
-        user_topic = st.text_input("Enter your Topic🔍")
-        no_of_news = st.slider('Number of News:', min_value=5, max_value=15, step=1)
+        user_topic = st.text_input("Enter your Topic")
+        no_of_news = st.slider('Number of News:', min_value=5, max_value=250, step=5)
 
         if st.button("Search") and user_topic != '':
             user_topic_pr = user_topic.replace(' ', '')
             news_list = fetch_news_search_topic(topic=user_topic_pr)
             if news_list:
-                st.subheader("✅ Here are the some {} News for you".format(user_topic.capitalize()))
+                st.subheader("Here are the some {} News for you".format(user_topic.capitalize()))
                 display_news(news_list, no_of_news)
             else:
                 st.error("No News found for {}".format(user_topic))
         else:
-            st.warning("Please write Topic Name to Search🔍")
+            st.warning("Please Select Topic For Search")
 
 
 run()
